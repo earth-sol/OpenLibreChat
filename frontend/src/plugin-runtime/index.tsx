b8cbb3c2e7ae9d3fdfc8188f6441b45ad3be0e83
earth-sol <55757024+earth-sol@users.noreply.github.com>
2025-04-25 20:31:00 +0800

    Create index.tsx

---
+++ frontend/src/plugin-runtime/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import PluginLoader from './plugin-runtime/PluginLoader';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <PluginLoader>
+      <App />
+    </PluginLoader>
+  </React.StrictMode>
+);
